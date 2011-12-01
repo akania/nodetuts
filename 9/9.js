d950f54157ea9d9a10033890c59bcc9767c9fdbe
Artur Kania <kaniartur@gmail.com>
2011-12-01 13:05:33 +0100

    new tuts

---
+++ 9/9.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.configure(function(){
+	
+	app.use(express.logger());
+	
+	app.use(express.errorHandler({
+		dumpExceptions : true,
+		showStack : true
+	}));
+	
+});
+
+app.set('views',__dirname + '/views' );
+app.set('view engine', 'jade' );
+app.set('view options', {layout:true})
+
+app.get('/',function( req, res ){
+	res.render('root');
+});
+
+app.listen(4000);
